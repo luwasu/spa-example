a95566dcfccf553fcf42663036dcc4aba0acf0be
Luis Santana-Holmes <luis.santanaholmes@gmail.com>
2022-02-19 15:56:37 +0100

    add quote page

---
+++ js/components/quote.js
@@ -0,0 +1,36 @@
+
+
+export default {
+    getTitle: function () {
+        return "Quote";
+    },
+
+    render: function () {
+        const $view = $($('#tpl-quote').html());
+
+        $('[data-action=button]', $view).click(e => {
+            e.preventDefault();
+            getQuote();
+        });
+
+        return $view;
+    }
+};
+
+function getQuote() {
+    let QUOTE_URL = 'https://locher.ti.bfh.ch/services/quote?JSON';
+    return $.ajax({
+        url: QUOTE_URL,
+        type: 'GET',
+        headers: {
+            'accept': 'application/json'
+        }
+    })
+        .then(data => $('[data-action=quote]').text(data.quote + ' -' + data.author))
+        .then(data => console.log('GET request successful'))
+        .catch(err => {
+            $('#joke').text('Sorry looks like something went wrong🥴. Please try again...');
+            console.log('Error Code: ' + err.status + ' ' + JSON.parse(err.responseText).message)
+        });
+}
+
--- js/main.js
+++ js/main.js
@@ -1,10 +1,12 @@
 import router from './router.js';
 import home from './components/home.js';
+import quote from "./components/quote.js";
 
 
 
 router.register('/', home);
 router.register('/home', home);
+router.register('/quote', quote);
 
 
 router.navigate('/');
